1f553e2c2e336c551a774bf117d945447abe5dce
somyajain27 <somyaj0169@gmail.com>
2025-04-14 13:02:13 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,29 +1,8 @@
-
-class Solution
-{
-public:
-    int getMinDiff(vector<int> &arr, int k)
-    {
-    }
-};
+#include<iostream>
+using namespace std;
 
 int main()
 {
-    cout << "enter number of elements in an array: ";
-    int n;
-    cin >> n;
-    vector<int> arr(n);
-    cout << "Enter elements: ";
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    cout << "enter height to increase or decrease: ";
-    int k;
-    cin >> k;
-    Solution s;
-    cout << "Maximum Profit: ";
-    cout << s.getMinDiff(arr, k);
-
+    
     return 0;
 }
